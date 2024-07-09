1418ab7bed47a3f2f4b43630f6199083d2cac44f
Matrix TG <108185896+TGxMATRIX@users.noreply.github.com>
2024-07-09 07:28:40 +0530

    Update channel.py

--- plugins/channel.py
+++ plugins/channel.py
@@ -1,6 +1,6 @@
 from pyrogram import Client, filters
 from info import CHANNELS
-from database.ia_filterdb import save_file
+from database.ia_filterdb import save_file2, save_file3, save_file4, save_file5, save_file6, save_file1, check_file
 
 media_filter = filters.document | filters.video | filters.audio
 
@@ -17,4 +17,40 @@ async def media(bot, message):
 
     media.file_type = file_type
     media.caption = message.caption
-    await save_file(media)
+    
+    if message.id % 6 == 0:
+        tru = await check_file(media)
+        if tru == "okda":
+            await save_file1(media)
+        else:
+            print("skipped duplicate file from saving to db 😌")
+    elif message.id % 6 == 1:
+        tru = await check_file(media)
+        if tru == "okda":
+            await save_file2(media)
+        else:
+            print("skipped duplicate file from saving to db 😌")
+    elif message.id % 6 == 2:
+        tru = await check_file(media)
+        if tru == "okda":
+            await save_file3(media)
+        else:
+            print("skipped duplicate file from saving to db 😌")
+    elif message.id % 6 == 3:
+        tru = await check_file(media)
+        if tru == "okda":
+            await save_file4(media)
+        else:
+            print("skipped duplicate file from saving to db 😌")
+    elif message.id % 6 == 4:
+        tru = await check_file(media)
+        if tru == "okda":
+            await save_file5(media)
+        else:
+            print("skipped duplicate file from saving to db 😌")
+    else:
+        tru = await check_file(media)
+        if tru == "okda":
+            await save_file6(media)
+        else:
+            print("skipped duplicate file from saving to db 😌")
